e30d97ffb17979dde98ba3fa91060202f8da5de0
lzlzx <luoxue_1130@163.com>
2020-12-08 12:44:25 +0800

    初始化laravel 项目提交版本库

---
+++ app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    //
+    function index()
+    {
+        return '这是后台控制器';
+    }
+}
---
+++ app/Http/Controllers/Home/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    //
+    function index()
+    {
+        return '这是前台控制器';
+    }
+
+
+}
